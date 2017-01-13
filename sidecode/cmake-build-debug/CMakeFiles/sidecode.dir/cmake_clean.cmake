f93e4c35fb33dbf22b6887c45c7cdef817e733e4
Robert Newton <newtorob@oregonstate.edu>
2017-01-13 15:57:04 -0500

    Lion likes to add files

---
+++ sidecode/cmake-build-debug/CMakeFiles/sidecode.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sidecode.dir/main.c.o"
+  "sidecode.pdb"
+  "sidecode"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sidecode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- sidecode/main.c
+++ sidecode/main.c
@@ -1,30 +1,18 @@
-//
-//  main.c
-//  sidecode
-//
-//  Created by Robert Newton on 1/12/17.
-//  Copyright © 2017 Robert Newton. All rights reserved.
-//
-
 #include <stdio.h>
-#include <limits.h>
 
-int main()
-{
+int main() {
     float in_tooCm, cm_tooIn;
     int cm, in;
-    
+
     cm = 5;
     in = 5;
-    
-    in_tooCm = in * 2.54;
-    cm_tooIn = cm/2.54;
-    
-    printf("inches to cm : %f \n", in_tooCm);
-    
-    
-    printf("centimeters to in : %f \n", cm_tooIn);
-    
-    return 0;
 
+    in_tooCm = in * 2.54;
+    cm_tooIn = cm / 2.54;
+
+    printf("inches to cm : %f cm \n ", in_tooCm);
+
+    printf("centimeters to in : %f in \n", cm_tooIn);
+
+    return 0;
 }
