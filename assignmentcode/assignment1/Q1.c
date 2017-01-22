90f9214b1b2c6ba6fe4808377c33073eeec22ebf
Robert Newton <newtorob@oregonstate.edu>
2017-01-22 12:24:00 -0500

    working on q1, saving progress

--- assignmentcode/assignment1/Q1.c
+++ assignmentcode/assignment1/Q1.c
@@ -15,13 +15,31 @@ struct student{
 
 struct student* allocate(){
      /*Allocate memory for ten students*/
-     
+     struct student* students = malloc(10 * sizeof(struct student));
      /*return the pointer*/
+     return students;
 }
 
 void generate(struct student* students){
      /*Generate random and unique ID and scores for ten students, ID being between 1 and 10, scores between 0 and 100*/
-     
+     int studentIds[10];
+
+     for(int i = 0; i < 10; i++)
+     {
+         studentIds[i] = i + 1;
+     }
+
+     for (int i = 0; i < 10; i++)
+     {
+         int index = rand() % 10;
+         while(studentIds[index] == 0)
+         {
+             index = rand() % 10;
+         }
+         students[i].id = studentIds[index];
+         students[i].score = rand() % 101;
+         studentIds[index] = 0;
+     }
 }
 
 void output(struct student* students){
@@ -33,6 +51,11 @@ void output(struct student* students){
               ID3 score3
               ...
               ID10 score10*/
+
+              for (int i = 0; i < 10; i++)
+              {
+                  printf("%d %d\n", students[i].id, students[i].score);
+              }
 }
 
 void summary(struct student* students){
